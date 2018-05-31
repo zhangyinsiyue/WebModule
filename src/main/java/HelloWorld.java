8d195c84eba6add53072b1e0b6f2ff67c12ef808
zhangyin <zhangyin@bsoft.com.cn>
2018-05-31 13:35:44 +0800

    增加了一行代码

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("1");
+        int a = 0;
+        int b= 1;
     }
 }
